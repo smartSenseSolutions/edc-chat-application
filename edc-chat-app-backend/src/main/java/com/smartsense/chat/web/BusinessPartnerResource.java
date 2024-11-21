4faa32717f63329d233acb7af77061e0d81848b9
Bhautik Sakhiya <bhautik.sakhiya@smartsensesolutions.com>
2024-11-21 16:39:46 +0530

    feat: remove unused api

--- edc-chat-app-backend/src/main/java/com/smartsense/chat/web/BusinessPartnerResource.java
+++ edc-chat-app-backend/src/main/java/com/smartsense/chat/web/BusinessPartnerResource.java
@@ -1,14 +1,11 @@
 package com.smartsense.chat.web;
 
-import com.smartsense.chat.dao.entity.BusinessPartner;
 import com.smartsense.chat.service.BusinessPartnerService;
 import com.smartsense.chat.utils.request.BusinessPartnerRequest;
 import com.smartsense.chat.utils.response.BpnResponse;
 import com.smartsense.chat.utils.response.BusinessPartnerResponse;
-import com.smartsensesolutions.commons.dao.filter.FilterRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,24 +19,19 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/partner")
+@RequestMapping("/api/partners")
 public class BusinessPartnerResource {
 
     private final BusinessPartnerService businessPartnerService;
 
-    @PostMapping(value = "/create", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public BusinessPartnerResponse createBusinessPartner(@RequestBody BusinessPartnerRequest request) {
         return businessPartnerService.createBusinessPartner(request);
     }
 
-    @GetMapping(value = "/get", produces = APPLICATION_JSON_VALUE)
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
     public BpnResponse getBusinessPartner(@RequestParam(required = false) String name) {
         return StringUtils.hasText(name) ? businessPartnerService.getBusinessPartner(name) : businessPartnerService.getAllBusinessPartners();
-    }
-
-    @PostMapping(value = "/fetch", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public Page<BusinessPartner> fetchRawData(@RequestBody FilterRequest request) {
-        return businessPartnerService.filter(request);
     }
 }
 
