ecf10ec267532e0e20d12ead05dc370176a8a4c2
Dilip Dhankecha <dilip.dhankecha@smartsensesolutions.com>
2024-11-28 15:47:21 +0530

    build: handle bp registration in docker deployment

---
+++ deployment/env/bp_registration/bp_registration.sh
@@ -0,0 +1,23 @@
+sleep 10
+
+echo "Initiate the BP Registration..."
+
+## Create partner data in smartSense
+curl --location 'http://smartsense-backend:8080/partners' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "Catena-X",
+    "edcUrl": "http://catena-x-controlplane:9195/api/v1/dsp",
+    "bpn": "BPNL00000CATENAX"
+}'
+
+## Create partner data in catena-x
+curl --location 'http://catena-x-backend:8080/partners' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "smartSense",
+    "edcUrl": "http://smartsense-controlplane:8195/api/v1/dsp",
+      "bpn": "BPNL00SMARTSENSE"
+}'
+
+echo "Completed the BP Registration..."
